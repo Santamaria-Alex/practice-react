a60405f331f6db759f763beb4bbf59ecef8c33f0
Alex Santamaria <santamariaalex146@gmail.com>
2022-01-14 11:34:16 -0600

    practicing w useState, map, and props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "TOOL",
+      day: "Feb 3rd at 7:00pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Megadeth",
+      day: "April 14 at 7:00pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Mourtuary Descent",
+      day: "Jan 15 at 10:00pm",
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
