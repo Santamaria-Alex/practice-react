b88dc5abaccea653832fbac6abba6c6c2b62e275
Alex Santamaria <santamariaalex146@gmail.com>
2022-01-15 20:36:46 -0600

    button changes text and color based on whether form is showing or not

--- src/App.js
+++ src/App.js
@@ -51,7 +51,10 @@ function App() {
 
   return (
     <div className="container">
-      <Header onAdd={() => setShotAddTask(!showAddTask)} />
+      <Header
+        onAdd={() => setShotAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
